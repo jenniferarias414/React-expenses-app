00e821abd8e57897e47af6f8deb330979faf454c
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2024-01-18 15:02:25 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//index.js is the first file to execute
+
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
