8382fdd4c75b6d55bb4f6ca122c9b97ef311c910
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2024-01-22 07:06:02 -0600

    chart bar completed

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css'
 import Card from '../UI/Card'
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 function Expenses(props){
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -22,7 +23,7 @@ function Expenses(props){
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear
   });
 
@@ -33,6 +34,7 @@ function Expenses(props){
             selected={filteredYear}
             onChangeFilter={filterChangeHandler}
           />
+          <ExpensesChart expenses={filteredExpenses} />
           {/* <p>Data for years {filterInfoText} is hidden.</p> */}
 
           {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
